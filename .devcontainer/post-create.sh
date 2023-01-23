0912f9ee02815048351a0b580846d63a53c8c3ba
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2023-01-23 09:49:10 +0000

    Update dev container config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -28,7 +28,7 @@ pip install metrixpp
  cp $HOME/.p10k-with-clock.zsh $HOME/.p10k.zsh
  cp $HOME/.zshrc $HOME/.zshrc.bak
  echo "$(cat $HOME/.zshrc)" | awk '{gsub(/ZSH_THEME=\"codespaces\"/, "ZSH_THEME=\"powerlevel10k\"")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
- echo "$(cat $HOME/.zshrc)" | awk '{gsub(/plugins=\(git\)/, "plugins=(git zsh-completions zsh-syntax-highlighting zsh-autosuggestions)")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
+ echo "$(cat $HOME/.zshrc)" | awk '{gsub(/plugins=\(git\)/, "plugins=(git zsh-completions zsh-syntax-highlighting zsh-autosuggestions dotnet)")}1' > $HOME/.zshrc.replaced && mv $HOME/.zshrc.replaced $HOME/.zshrc
  echo "
  # To customize prompt, run 'p10k configure' or edit ~/.p10k.zsh.
  [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
